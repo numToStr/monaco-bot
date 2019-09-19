29c73fa5f0d2738fe07ed644e4b798bea3095bec
vkasraj <vikasraj1911@gmail.com>
2019-09-19 14:13:21 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Editor from "./Editor/Editor";
+import Bot from "./Bot/Bot";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Grid container>
+            <Grid item xs={6}>
+                <Editor />
+            </Grid>
+            <Grid item xs={6}>
+                <Bot />
+            </Grid>
+        </Grid>
+    );
+};
 
 export default App;
