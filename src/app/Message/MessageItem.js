4524178864d94523ddbc7a25eb1f8a0ab9846ad6
vkasraj <vikasraj1911@gmail.com>
2019-09-23 22:09:38 +0530

    added avatar in message list

--- src/app/Message/MessageItem.js
+++ src/app/Message/MessageItem.js
@@ -1,5 +1,5 @@
 import React, { memo } from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Avatar } from "@material-ui/core";
 
 const MessageItem = ({ node }) => {
     const isUser = node.createdBy === "user";
@@ -10,15 +10,18 @@ const MessageItem = ({ node }) => {
             justifyContent={isUser ? "flex-end" : "flex-start"}
             my={2}
         >
+            {!isUser && <Avatar>B</Avatar>}
             <Box
                 bgcolor={isUser ? "primary.dark" : "primary.main"}
                 color="white"
                 borderRadius="borderRadius"
                 py={1}
                 px={2}
+                mx={2}
             >
                 <Typography>{node.message}</Typography>
             </Box>
+            {isUser && <Avatar>U</Avatar>}
         </Box>
     );
 };
